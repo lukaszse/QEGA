8fa89b4936bbf1a557be1c09da87f3cd443f3c88
lukaszse <llseremak@gmail.com>
2021-11-06 12:04:18 +0100

    implement SelectionService

--- src/main/java/pl/com/seremak/service/SelectionService.java
+++ src/main/java/pl/com/seremak/service/SelectionService.java
@@ -19,12 +19,20 @@ public class SelectionService {
         this.c = c;
     }
 
-    public double calculateFunctionValueSum(List<Individual> population) {
+    public double calculateFunctionValueSum(final List<Individual> population) {
         return population
                 .map(Individual::toInt)
                 .map(individualValue ->
                         QuadraticEquation.calculateValue(individualValue, a, b, c))
                 .sum()
                 .doubleValue();
+    }
+
+    public double individualSelectionProbability(final List<Individual> population, final Individual individual) {
+        var functionSumValue = calculateFunctionValueSum(population);
+        var individualValue = QuadraticEquation.calculateValue(individual.toInt(), a, b, c);
+        return functionSumValue != 0 || individualValue != 0 ?
+                individualValue / calculateFunctionValueSum(population) :
+                0;
     }
 }
--- src/test/groovy/pl/com/seremak/service/SelectionServiceTest.groovy
+++ src/test/groovy/pl/com/seremak/service/SelectionServiceTest.groovy
@@ -16,12 +16,7 @@ class SelectionServiceTest extends Specification {
     def 'should calculate sum of function values for given population'() {
 
         given: 'prepare individuals'
-        def individual1 = Individual.of(List.of(false, false, false, false, false, false, false, true))
-        def individual2 = Individual.of(List.of(false, false, false, false, false, false, true, false))
-        def individual3 = Individual.of(List.of(false, false, false, false, false, false, true, true))
-        def individual4 = Individual.of(List.of(false, true, false, false, false, false, true, true))
-
-        def population = List.of(individual1, individual2, individual3, individual4)
+        def population = preparePopulation()
 
         and: 'set input quadratic function factors'
         selectionService.setQuadraticEquationFactors(a, b, c)
@@ -38,5 +33,36 @@ class SelectionServiceTest extends Specification {
         3 | 1 | 0 | 13582
         2 | 0 | 0 | 9006
         0 | 0 | 0 | 0
+    }
+
+    def 'should calculate probability of individual selection'() {
+
+        given: 'prepare individuals'
+        def population = preparePopulation()
+
+        and: 'set input quadratic function factors'
+        selectionService.setQuadraticEquationFactors(a, b, c)
+
+        when: 'calculate individual selection probability'
+        def probability = selectionService.individualSelectionProbability(population, population.get(1))
+
+        then:
+        probability.round(4) == expectedProbability
+
+        where:
+        a | b | c | expectedProbability
+        2 | 2 | 2 | 0.0015
+        3 | 1 | 0 | 0.0010
+        2 | 0 | 0 | 0.0009
+        0 | 0 | 0 | 0
+    }
+
+
+    static def preparePopulation() {
+        def individual1 = Individual.of(List.of(false, false, false, false, false, false, false, true))
+        def individual2 = Individual.of(List.of(false, false, false, false, false, false, true, false))
+        def individual3 = Individual.of(List.of(false, false, false, false, false, false, true, true))
+        def individual4 = Individual.of(List.of(false, true, false, false, false, false, true, true))
+        return List.of(individual1, individual2, individual3, individual4)
     }
 }
